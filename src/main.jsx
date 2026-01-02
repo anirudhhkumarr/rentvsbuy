9e7de5adcac391cab1c55c3f6367763a978bae9c
Anirudh <anirudh@local>
2026-01-01 17:59:46 -0800

    fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { HashRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import App from "./App.jsx"
 import "./assets/css/index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter basename="/rentvsbuy" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
             <App />
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>
 )
